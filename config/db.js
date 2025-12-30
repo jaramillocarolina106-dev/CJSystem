17756b59b63c91c517aecc2d952e256f5cf23a29
jaramillocarolina106-dev <jaramillocarolina106@gmail.com>
2025-12-30 17:44:49 -0500

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,1 +1,11 @@
+import mongoose from "mongoose";
 
+export const conectarDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("🔥 MongoDB conectado");
+  } catch (error) {
+    console.error("❌ Error DB:", error);
+    process.exit(1);
+  }
+};
