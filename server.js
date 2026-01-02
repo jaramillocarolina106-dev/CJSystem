a26a91d72ef3e3b536e30875842860c537b2d2e2
jaramillocarolina106-dev <jaramillocarolina106@gmail.com>
2026-01-02 12:02:08 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -95,6 +95,8 @@ app.use("/api/usuarios", require("./routes/usuariosRoutes"));
 app.use("/api/config-empresa", configEmpresaRoutes);
 app.use("/api/reportes", require("./routes/reportesRoutes"));
 app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/superadmin", require("./routes/superadminEmpresaRoutes"));
+
 
 // =========================
 // 📁 ESTÁTICOS
